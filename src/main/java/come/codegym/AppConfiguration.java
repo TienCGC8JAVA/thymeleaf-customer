332a206942522b5be96b34491c1005a5c98218bd
TienCGC8JAVA <nguyenmanhtien08081994@gmail.com>
2019-07-02 17:43:26 +0700

    add beans for ApplicationContext

--- src/main/java/come/codegym/AppConfiguration.java
+++ src/main/java/come/codegym/AppConfiguration.java
@@ -3,10 +3,23 @@ package come.codegym;
 import org.springframework.beans.BeansException;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.ApplicationContextAware;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.thymeleaf.spring4.templateresolver.SpringResourceTemplateResolver;
 
+@Configuration
 public class AppConfiguration implements ApplicationContextAware {
+
+    @Bean
+    public SpringResourceTemplateResolver springResourceTemplateResolver() {
+        SpringResourceTemplateResolver templateResolver = new SpringResourceTemplateResolver();
+        templateResolver.setApplicationContext();
+    }
+
+    private ApplicationContext applicationContext;
+
     @Override
     public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
-
+        this.applicationContext = applicationContext;
     }
 }
