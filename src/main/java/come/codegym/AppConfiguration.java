c4c15e86aecbbeab975cfbd28fd6f9f3ce7ecac6
TienCGC8JAVA <nguyenmanhtien08081994@gmail.com>
2019-07-02 16:25:04 +0700

    AppConfiguration have interface ApplicationContextAware

--- src/main/java/come/codegym/AppConfiguration.java
+++ src/main/java/come/codegym/AppConfiguration.java
@@ -1,4 +1,12 @@
 package come.codegym;
 
-public class AppConfiguration {
+import org.springframework.beans.BeansException;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
+
+public class AppConfiguration implements ApplicationContextAware {
+    @Override
+    public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
+
+    }
 }
